ed5737bc1c26ab814263d866daa5c84d3b17ffd2
nguyenvu1892 <nguyenvu1892@gmail.com>
2026-04-09 22:51:00 +0700

    fix: ORDER_FILLING_FOK for Exness Pro on all branches (v2 hotfix)

--- live_bot.py
+++ live_bot.py
@@ -431,7 +431,7 @@ def send_market_order(symbol: str, direction: str, lot: float, magic: int) -> bo
         "magic": magic,
         "comment": "ScalpEx200_v2",
         "type_time": mt5.ORDER_TIME_GTC,
-        "type_filling": mt5.ORDER_FILLING_IOC,
+        "type_filling": mt5.ORDER_FILLING_FOK,
     }
 
     result = mt5.order_send(request)
@@ -468,7 +468,7 @@ def close_position(position, symbol: str) -> float:
         "magic": position.magic,
         "comment": "ScalpEx200_Close",
         "type_time": mt5.ORDER_TIME_GTC,
-        "type_filling": mt5.ORDER_FILLING_IOC,
+        "type_filling": mt5.ORDER_FILLING_FOK,
     }
 
     result = mt5.order_send(request)
